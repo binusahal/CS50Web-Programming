3435f3f12e62ebb34dfbe196dc677569bc982c95
sahal.Tech <sahal.Tech@gmail.com>
2023-09-11 16:55:31 +0300

    CS50W Project 4

---
+++ project 4/network/urls.py
@@ -0,0 +1,19 @@
+
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
+    path("newPost", views.newPost, name="newPost"),
+    path("profile/<int:user_id>", views.profile, name="profile"),
+    path("ufollow", views.unfollow, name="unfollow"),
+    path("follow", views.follow, name="follow"),
+    path("following", views.following, name="following"),
+    path("edit/<int:post_id>", views.edit, name="edit"),
+    path("removeLike/<int:post_id>", views.removeLike, name="removeLike"),
+    path("addLike/<int:post_id>", views.addLike, name="addLike"),
+]
